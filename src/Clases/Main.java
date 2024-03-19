650f4281a77f96d7e9a303df0064e219a09fad56
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2024-03-18 21:41:45 -0400

    Ventanas, repuesto

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -1,10 +1,12 @@
 
 package Clases;
+import Ventanas.V1Inicio;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+       V1Inicio v1 = new V1Inicio(); 
+       v1.setVisible(true);
     }
     
 }
