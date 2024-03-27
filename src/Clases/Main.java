d901e39a9079c9cbb6d921bf481ba96a21c330ea
josedaniel5092003 <jose-fernandez@correo.unimet.edu.ve>
2024-03-27 00:33:09 -0400

    Ventanas 1.1
    Ventanas actualizadas.

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -1,12 +1,10 @@
 
 package Clases;
-import Ventanas.V1Inicio;
 
 public class Main {
 
     public static void main(String[] args) {
-       V1Inicio v1 = new V1Inicio(); 
-       v1.setVisible(true);
+       
     }
     
 }
